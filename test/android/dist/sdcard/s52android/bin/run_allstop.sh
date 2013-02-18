e073bfbccc6bf97b9ec203e487e77c6f90be355a
sduclos <sduclos@Acer-Laptop.(none)>
2013-02-18 05:12:09 -0500

    change sdcard path

--- test/android/dist/sdcard/s52android/bin/run_allstop.sh
+++ test/android/dist/sdcard/s52android/bin/run_allstop.sh
@@ -4,14 +4,14 @@
 
 
 # signal sl4agps for a clean exit
-kill -SIGINT `cat /data/media/s52android/bin/sl4agps.pid 2> /dev/null` 2> /dev/null
+kill -SIGINT `cat /sdcard/s52android/bin/sl4agps.pid 2> /dev/null` 2> /dev/null
 # debug -
-rm /data/media/s52android/bin/sl4agps.pid
+rm /sdcard/s52android/bin/sl4agps.pid
 
 # stop sl4a server
 /system/xbin/su -c "/system/bin/am force-stop com.googlecode.android_scripting"
 #/system/xbin/su -c "kill -9  `ps | grep com.googlecode.android_scripting | awk '{ print $2 }'`"
 
 # signal s52ais for a clean exit
-kill -SIGINT `cat /data/media/s52android/bin/s52ais.pid 2> /dev/null` 2> /dev/null
-rm /data/media/s52android/bin/s52ais.pid
+kill -SIGINT `cat /sdcard/s52android/bin/s52ais.pid 2> /dev/null` 2> /dev/null
+rm /sdcard/s52android/bin/s52ais.pid
