cdf2ba94a2729b1265bd96ad729a3051a1c62ec1
sduclos <sduclos@Acer-Laptop>
2014-11-30 12:35:35 -0500

    del toggleObjClass/ON/OFF

--- test/s52gtk3egl.c
+++ test/s52gtk3egl.c
@@ -493,9 +493,14 @@ static int      _s52_setupVRMEBL(s52droid_state_t *state)
     //_vrmeblA = S52_newVRMEBL(S52_VRMEBL_vrm, !S52_VRMEBL_ebl, S52_VRMEBL_sty, !S52_VRMEBL_ori);
     //_vrmeblA = S52_newVRMEBL(S52_VRMEBL_vrm, !S52_VRMEBL_ebl, S52_VRMEBL_sty,  S52_VRMEBL_ori);
 
-    S52_toggleObjClassON("cursor");  // suppression ON
-    S52_toggleObjClassON("ebline");
-    S52_toggleObjClassON("vrmark");
+    //S52_toggleObjClassON("cursor");  // suppression ON
+    //S52_toggleObjClassON("ebline");
+    //S52_toggleObjClassON("vrmark");
+
+    // suppression ON
+    S52_setS57ObjClassSupp("cursor", TRUE);
+    S52_setS57ObjClassSupp("ebline", TRUE);
+    S52_setS57ObjClassSupp("vrmark", TRUE);
 
     return TRUE;
 }
@@ -609,8 +614,9 @@ static int      _s52_init       (s52engine *engine)
     // debug - remove clutter from this symb in SELECT mode
     //S52_setS57ObjClassSupp("M_QUAL", TRUE);  // supress display of the U pattern
     //S52_setS57ObjClassSupp("M_QUAL", FALSE);  // displaythe U pattern
-    S52_toggleObjClassON ("M_QUAL");           //  suppression ON
+    //S52_toggleObjClassON ("M_QUAL");           //  suppression ON
     //S52_toggleObjClassOFF("M_QUAL");         //  suppression OFF
+    S52_setS57ObjClassSupp("M_QUAL", TRUE);
 
 
     S52_loadPLib(PLIB);
