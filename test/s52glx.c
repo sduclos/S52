dbd451c214dde3a91fc003981f565de9d794d689
sduclos <sduclos@Acer-Laptop>
2014-12-27 06:58:52 -0500

    add ESC to exit

--- test/s52glx.c
+++ test/s52glx.c
@@ -25,11 +25,14 @@
 
 #include "S52.h"
 
-#include <X11/Xlib.h>   // X*()
-#include <GL/glx.h>     // glX*()
+#include <X11/Xlib.h>          // X*()
+#include <X11/XKBlib.h>        // Xkb*()
+#include <X11/keysymdef.h>
 
-#include <stdio.h>      // printf()
-#include <stdlib.h>     // exit(0)
+#include <GL/glx.h>            // glX*()
+
+#include <stdio.h>             // printf()
+#include <stdlib.h>            // exit()
 
 #define WIDTH  800
 #define HEIGHT 600
@@ -99,7 +102,9 @@ static Window       _setXwin(Display *dpy, XVisualInfo *visInfo)
 
     swa.colormap     = cmap;
     swa.border_pixel = 0;
-    swa.event_mask   = ExposureMask | ButtonPressMask | StructureNotifyMask;
+    //swa.event_mask   = ExposureMask | ButtonPressMask | StructureNotifyMask;
+    //swa.event_mask   = ExposureMask | StructureNotifyMask | KeyPressMask | ButtonPressMask | ButtonReleaseMask | PointerMotionMask;
+    swa.event_mask   = ExposureMask | StructureNotifyMask | KeyPressMask | ButtonPressMask | ButtonReleaseMask;
 
     win = XCreateWindow(dpy, win, 
                         0, 0, WIDTH, HEIGHT, 0, visInfo->depth,
@@ -179,15 +184,29 @@ int main(int argc, char* argv[])
                 //    S52_setViewPort(0, 0, event.xconfigure.width, event.xconfigure.height); break;
                 //case ...
 
+                case KeyPress:
+                case KeyRelease:
+                    {
+                        unsigned int keycode = ((XKeyEvent *)&event)->keycode;
+                        unsigned int keysym  = XkbKeycodeToKeysym(dpy, keycode, 0, 1);
+
+                        // quit
+                        if (XK_Escape == keysym || XK_q == keysym) {
+                            goto exit;
+                        }
+                    }
                 }
             } while (XPending(dpy));
             S52_draw();
-            // FIXME
-            S52_drawLast();
+            S52_drawLast();  // nothing to do
 
             glXSwapBuffers(dpy,  win);
         }
     }
+
+exit:
+
+    XKillClient(dpy, win);
 
     S52_done();
 
