8801e8ec6e2fd20aa502541620ded2ace5d6013e
sduclos <sduclos@Acer-Laptop>
2015-01-28 14:10:43 -0500

    fix CLang warning, update version to libS52-2015JAN28-1.159

--- S52utils.c
+++ S52utils.c
@@ -55,7 +55,7 @@ static GTimeVal _now;
 #endif
 
 // internal libS52.so version
-static char _version[] = "libS52-2015JAN19-1.158"
+static char _version[] = "libS52-2015JAN28-1.159"
 #ifdef _MINGW
       ",_MINGW"
 #endif
