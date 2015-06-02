d35551c00305c74868cfaf9545790e0a927e7729
sduclos <sduclos@Acer-Laptop>
2015-06-02 12:47:30 -0400

    fix comment

--- test/s52ui/web/s52.dart
+++ test/s52ui/web/s52.dart
@@ -4,7 +4,7 @@ part of s52ui;
 //////////////////////////////////////////////////////
 //
 // Base Class that mimic S52 interface (S52.h)
-// UI <--> WebSocket (s52ui.html)
+// UI <--> WebSocket <--> libS52.so
 //
 
 class Cmd {
@@ -17,7 +17,6 @@ class Cmd {
   }
 }
 
-//abstract class S52 {
 class S52 {
   Map _data = JSON.decode('{"id":1,"method":"???","params":["???"]}');
   int _id   = 1;
@@ -33,6 +32,8 @@ class S52 {
   bool  _skipTimer = false;
   set skipTimer(val) => _skipTimer = val;
 
+  // convolute way to handle ordering of snd/rcv Cmd
+  // the new await/async could simplify the logic
   Queue<Cmd> _queue = new Queue<Cmd>();
 
   // S52 color for UI element
@@ -82,7 +83,7 @@ class S52 {
   static const int MAR_DISP_LAYER_LAST_NONE   =        8;  // 1 << 3; 0001000
   static const int MAR_DISP_LAYER_LAST_STD    =       16;  // 1 << 4; 0010000
   static const int MAR_DISP_LAYER_LAST_OTHER  =       32;  // 1 << 5; 0100000
-  static const int MAR_DISP_LAYER_LAST_SELECT =       64;  // 1 << 5; 1000000
+  static const int MAR_DISP_LAYER_LAST_SELECT =       64;  // 1 << 6; 1000000
 
   static const int CMD_WRD_FILTER             = 33;
   static const int CMD_WRD_FILTER_SY          =        1;  // 1 << 0; 000001 - SY
@@ -92,7 +93,7 @@ class S52 {
   static const int CMD_WRD_FILTER_AP          =       16;  // 1 << 4; 010000 - AP
   static const int CMD_WRD_FILTER_TX          =       32;  // 1 << 5; 100000 - TE & TX
 
-  S52() {}
+  S52() {}  // init
 
   Future<bool> initWS(var wsUri) {
     Completer completer = new Completer();
@@ -111,10 +112,10 @@ class S52 {
     if (null != _timer)
       return;
 
-    // call drawLast every second (1sec)
+    // call drawLast every second (1000ms)
     _timer = new Timer.periodic(new Duration(milliseconds: 1000), (timer) {
       if (false == _skipTimer) {
-        drawLast().then((ret) {});
+        drawLast();
       }
     });
   }
@@ -155,7 +156,6 @@ class S52 {
         throw "rcvMsg(): ID mismatch";
       }
 
-      // drawLast()
       _skipTimer = false;
 
       return cmd._completer.complete(data['result']);
@@ -183,8 +183,11 @@ class S52 {
 
   /*
   // alternate way of calling libS52 - one call for all S52.h calls
+  // pro: less code
+  // con: lost of info .. doesn't mirror S52.h well since all call info would be
+  // littered across s52ui.dart
   Future<List> send(var cmdName, var params) {
-    _data["id"    ] = _id;
+    _data["id"    ] = id;
     _data["method"] = cmdName;
     _data["params"] = params;
     String jsonCmdstr = JSON.encode(_data);
@@ -193,10 +196,6 @@ class S52 {
   }
   */
 
-  ////////////////////// FIXME: REFACTOR use send() ///////////////////////////
-  // pro: less code
-  // con: lost of info .. doesn't mirror S52.h well since all call info would be
-  // littered across s52ui.dart
   Future<List> draw() {
     _data["id"    ] = id;
     _data["method"] = "S52_draw";
