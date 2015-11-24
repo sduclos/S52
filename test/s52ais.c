0d05248f76252b5a8a12411adf033b4cc11f995e
sduclos <sduclos@Acer-Laptop>
2015-11-24 14:37:50 -0500

    HOST IP change

--- test/s52ais.c
+++ test/s52ais.c
@@ -209,9 +209,9 @@ static struct sigaction _old_signal_handler_SIGTERM;
 #endif
 
 #ifdef S52_USE_ANDROID
-//#define GPSD_HOST "192.168.1.66"  // connect to GPSD on local network
+#define GPSD_HOST "192.168.1.66"  // connect to GPSD on local network
 //#define GPSD_HOST "192.168.1.68"  // connect to GPSD on local network
-#define GPSD_HOST "192.168.1.70"  // connect to GPSD on local network
+//#define GPSD_HOST "192.168.1.70"  // connect to GPSD on local network
 #else
 #define GPSD_HOST "localhost"     // connect to local GPSD
 #endif
