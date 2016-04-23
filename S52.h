77d1f6a763aed342c738ea4e2a0d0e775a24c646
sduclos <sduclos@Acer-Laptop>
2016-04-23 16:01:51 -0400

    update version to libS52-2016APR23-1.176

--- S52.h
+++ S52.h
@@ -194,7 +194,7 @@ typedef enum S52_MAR_DISP_CATEGORY_t {
  *
  * Return: (transfer none): String with the version of libS52 and the '#define' used to build it
  */
-#define S52_VERSION "libS52-2016APR09-1.175"
+#define S52_VERSION "libS52-2016APR23-1.176"
 DLL const char * STD S52_version(void);
 
 /**
