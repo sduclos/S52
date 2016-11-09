51356f8f97d30482d8a794b66abd5ad651a4f745
sduclos <sduclos@users.sf.net>
2016-11-09 09:04:43 -0500

    fix clang warning, stage gtk3 gesture

--- test/s52gtkegl.c
+++ test/s52gtkegl.c
@@ -363,39 +363,31 @@ static gboolean _rotation(GdkEventKey *event)
 
 static gboolean _toggle  (S52MarinerParameter paramName)
 {
-    double val = 0.0;
-
-    val = S52_getMarinerParam(paramName);
-    val = S52_setMarinerParam(paramName, !val);
+    double val = S52_getMarinerParam(paramName);
+    S52_setMarinerParam(paramName, !val);
 
     return TRUE;
 }
 
 static gboolean _meterInc(S52MarinerParameter paramName)
 {
-    double val = 0.0;
-
-    val = S52_getMarinerParam(paramName);
-    val = S52_setMarinerParam(paramName, ++val);
+    double val = S52_getMarinerParam(paramName);
+    S52_setMarinerParam(paramName, ++val);
 
     return TRUE;
 }
 
 static gboolean _meterDec(S52MarinerParameter paramName)
 {
-    double val = 0.0;
-
-    val = S52_getMarinerParam(paramName);
-    val = S52_setMarinerParam(paramName, --val);
+    double val = S52_getMarinerParam(paramName);
+    S52_setMarinerParam(paramName, --val);
 
     return TRUE;
 }
 
-static gboolean _disp    (S52MarinerParameter paramName, const char disp)
+static gboolean _disp    (S52MarinerParameter paramName, int disp)
 {
-    double val = (double) disp;
-
-    val = S52_setMarinerParam(paramName, val);
+    S52_setMarinerParam(paramName, disp);
 
     return TRUE;
 }
@@ -403,30 +395,23 @@ static gboolean _disp    (S52MarinerParameter paramName, const char disp)
 static gboolean _cpal    (S52MarinerParameter paramName, double val)
 {
     val = S52_getMarinerParam(paramName) + val;
-
-    val = S52_setMarinerParam(paramName, val);
+    S52_setMarinerParam(paramName, val);
 
     return TRUE;
 }
 
 static gboolean _inc     (S52MarinerParameter paramName)
 {
-    double val = 0.0;
-
-    val = S52_getMarinerParam(paramName) + 15.0;
-
-    val = S52_setMarinerParam(paramName, val);
+    double val = S52_getMarinerParam(paramName) + 15.0;
+    S52_setMarinerParam(paramName, val);
 
     return TRUE;
 }
 
 static gboolean _mmInc   (S52MarinerParameter paramName)
 {
-    double val = 0.0;
-
-    val = S52_getMarinerParam(paramName) + 0.01;
-
-    val = S52_setMarinerParam(paramName, val);
+    double val = S52_getMarinerParam(paramName) + 0.01;
+    S52_setMarinerParam(paramName, val);
 
     return TRUE;
 }
@@ -583,6 +568,173 @@ static gboolean key_release_event(GtkWidget   *widget,
     return TRUE;
 }
 
+/*
+//////////////////////////////////////////////////
+// code lifted from gtk/demos/gtk-demo/gesture.c
+//
+
+// Gestures
+
+//  Perform gestures on touchscreens and other input devices. This
+//  demo reacts to long presses and swipes from all devices, plus
+//  multi-touch rotate and zoom gestures.
+
+static GtkGesture *rotate = NULL;
+static GtkGesture *zoom = NULL;
+static gdouble swipe_x = 0;
+static gdouble swipe_y = 0;
+static gboolean long_pressed = FALSE;
+
+static void     swipe_gesture_swept(GtkGestureSwipe *gesture,
+                                    gdouble          velocity_x,
+                                    gdouble          velocity_y,
+                                    GtkWidget       *widget)
+{
+    swipe_x = velocity_x / 10;
+    swipe_y = velocity_y / 10;
+    gtk_widget_queue_draw (widget);
+}
+
+static void     long_press_gesture_pressed(GtkGestureLongPress *gesture,
+                                           gdouble              x,
+                                           gdouble              y,
+                                           GtkWidget           *widget)
+{
+    long_pressed = TRUE;
+    gtk_widget_queue_draw (widget);
+}
+
+static void     long_press_gesture_end(GtkGesture       *gesture,
+                                       GdkEventSequence *sequence,
+                                       GtkWidget        *widget)
+{
+    long_pressed = FALSE;
+    gtk_widget_queue_draw (widget);
+}
+
+static void     rotation_angle_changed(GtkGestureRotate *gesture,
+                                       gdouble           angle,
+                                       gdouble           delta,
+                                       GtkWidget        *widget)
+{
+    gtk_widget_queue_draw (widget);
+}
+
+static void     zoom_scale_changed(GtkGestureZoom *gesture,
+                                   gdouble         scale,
+                                   GtkWidget      *widget)
+{
+    gtk_widget_queue_draw (widget);
+}
+
+static gboolean drawing_area_draw(GtkWidget *widget,
+                                  cairo_t   *cr)
+{
+    GtkAllocation allocation;
+
+    gtk_widget_get_allocation (widget, &allocation);
+
+    if (swipe_x != 0 || swipe_y != 0) {
+        cairo_save (cr);
+        cairo_set_line_width (cr, 6);
+        cairo_move_to (cr, allocation.width / 2, allocation.height / 2);
+        cairo_rel_line_to (cr, swipe_x, swipe_y);
+        cairo_set_source_rgba (cr, 1, 0, 0, 0.5);
+        cairo_stroke (cr);
+        cairo_restore (cr);
+    }
+
+    if (gtk_gesture_is_recognized (rotate) || gtk_gesture_is_recognized (zoom)) {
+        cairo_pattern_t *pat;
+        cairo_matrix_t matrix;
+        gdouble angle, scale;
+
+        cairo_get_matrix (cr, &matrix);
+        cairo_matrix_translate (&matrix, allocation.width / 2, allocation.height / 2);
+
+        cairo_save (cr);
+
+        angle = gtk_gesture_rotate_get_angle_delta (GTK_GESTURE_ROTATE (rotate));
+        cairo_matrix_rotate (&matrix, angle);
+
+        scale = gtk_gesture_zoom_get_scale_delta (GTK_GESTURE_ZOOM (zoom));
+        cairo_matrix_scale (&matrix, scale, scale);
+
+        cairo_set_matrix (cr, &matrix);
+        cairo_rectangle (cr, -100, -100, 200, 200);
+
+        pat = cairo_pattern_create_linear (-100, 0, 200, 0);
+        cairo_pattern_add_color_stop_rgb (pat, 0, 0, 0, 1);
+        cairo_pattern_add_color_stop_rgb (pat, 1, 1, 0, 0);
+        cairo_set_source (cr, pat);
+        cairo_fill (cr);
+
+        cairo_restore (cr);
+
+        cairo_pattern_destroy (pat);
+    }
+
+    if (long_pressed) {
+        cairo_save (cr);
+        cairo_arc (cr, allocation.width / 2, allocation.height / 2, 50, 0, 2 * G_PI);
+
+        cairo_set_source_rgba (cr, 0, 1, 0, 0.5);
+        cairo_stroke (cr);
+
+        cairo_restore (cr);
+    }
+
+    return TRUE;
+}
+
+static int      _gtk_init_gestures(GtkWidget *window)
+{
+    //static GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    //gtk_window_set_default_size (GTK_WINDOW (window), 400, 400);
+    //gtk_window_set_title (GTK_WINDOW (window), "Gestures");
+
+    GtkWidget *drawing_area = gtk_drawing_area_new ();
+    gtk_container_add (GTK_CONTAINER (window), drawing_area);
+    gtk_widget_add_events (drawing_area,
+                           GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK |
+                           GDK_POINTER_MOTION_MASK | GDK_TOUCH_MASK);
+
+    g_signal_connect (drawing_area, "draw", G_CALLBACK (drawing_area_draw), NULL);
+
+    // Swipe
+    GtkGesture *gesture = gtk_gesture_swipe_new (drawing_area);
+    g_signal_connect (gesture, "swipe", G_CALLBACK (swipe_gesture_swept), drawing_area);
+    gtk_event_controller_set_propagation_phase (GTK_EVENT_CONTROLLER (gesture),
+                                                GTK_PHASE_BUBBLE);
+    g_object_weak_ref (G_OBJECT (drawing_area), (GWeakNotify) g_object_unref, gesture);
+
+    // Long press
+    gesture = gtk_gesture_long_press_new (drawing_area);
+    g_signal_connect (gesture, "pressed", G_CALLBACK (long_press_gesture_pressed), drawing_area);
+    g_signal_connect (gesture, "end", G_CALLBACK (long_press_gesture_end), drawing_area);
+    gtk_event_controller_set_propagation_phase (GTK_EVENT_CONTROLLER (gesture),
+                                                    GTK_PHASE_BUBBLE);
+    g_object_weak_ref (G_OBJECT (drawing_area), (GWeakNotify) g_object_unref, gesture);
+
+    // Rotate
+    rotate = gesture = gtk_gesture_rotate_new (drawing_area);
+    g_signal_connect (gesture, "angle-changed", G_CALLBACK (rotation_angle_changed), drawing_area);
+    gtk_event_controller_set_propagation_phase (GTK_EVENT_CONTROLLER (gesture),
+                                                GTK_PHASE_BUBBLE);
+    g_object_weak_ref (G_OBJECT (drawing_area), (GWeakNotify) g_object_unref, gesture);
+
+    // Zoom
+    zoom = gesture = gtk_gesture_zoom_new (drawing_area);
+    g_signal_connect (gesture, "scale-changed", G_CALLBACK (zoom_scale_changed), drawing_area);
+    gtk_event_controller_set_propagation_phase (GTK_EVENT_CONTROLLER (gesture),
+                                                GTK_PHASE_BUBBLE);
+    g_object_weak_ref (G_OBJECT (drawing_area), (GWeakNotify) g_object_unref, gesture);
+
+
+    return TRUE;
+}
+*/
+
 static int      _gtk_init(int argc, char** argv)
 {
     gtk_init(&argc, &argv);
@@ -596,11 +748,14 @@ static int      _gtk_init(int argc, char** argv)
     gtk_widget_set_double_buffered   (GTK_WIDGET(_engine.window), FALSE);
     gtk_widget_set_redraw_on_allocate(GTK_WIDGET(_engine.window), TRUE );
 
+    //g_signal_connect(G_OBJECT(_engine.window), "destroy",           G_CALLBACK(gtk_widget_destroyed), &window);
     g_signal_connect(G_OBJECT(_engine.window), "destroy",           G_CALLBACK(gtk_main_quit),     NULL);
     g_signal_connect(G_OBJECT(_engine.window), "key_release_event", G_CALLBACK(key_release_event), NULL);
     g_signal_connect(G_OBJECT(_engine.window), "configure_event",   G_CALLBACK(configure_event),   NULL);
 
     g_timeout_add(500, _s52_draw_cb, &_engine); // 0.5 sec
+
+    //_gtk_init_gestures(_engine.window);
 
     gtk_widget_show_all(_engine.window);
 
