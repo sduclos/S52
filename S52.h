296be2f3e3b96dd38b68d6e713241bd17539d343
sduclos <sduclos@users.sf.net>
2017-03-31 12:44:21 -0400

    update version to libS52-2017MAR31-1.203

--- S52.h
+++ S52.h
@@ -198,7 +198,7 @@ typedef enum S52_MAR_DISP_CATEGORY_t {
  *
  * Return: (transfer none): String with the version of libS52 and the '#define' used to build it
  */
-#define S52_VERSION "libS52-2017MAR18-1.202"
+#define S52_VERSION "libS52-2017MAR31-1.203"
 DLL const char * STD S52_version(void);
 
 /**
